dad9f4b2c47ec0696227345160820c32ed16bd7d
Josh Matthews <josh@joshmatthews.net>
2013-06-18 22:07:47 +0200

    Add nice summaries and icons.

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,25 @@
 //var API_HOST = "http://tranquil-plateau-4519.herokuapp.com/";
 var API_HOST = "cgi-bin/query.cgi?url=";
 
+var icons = {
+  'bugzilla': 'bugzilla.gif',
+  'hg': 'hg.png'
+};
+
+function nice_description(item) {
+  switch (item.source) {
+    case "bugzilla":
+      if ('comment' in item.extra) {
+        return "Commented on bug " + item.extra.id;
+      } else {
+        return "Modified bug " + item.extra.id;
+      }
+      break;
+    case "hg":
+      return "Committed hg revision.";
+  }
+}
+
 function fetch(email) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function() {
@@ -12,7 +31,13 @@ function fetch(email) {
     for (var i = 0; i < items.length; i++) {
       var r = document.createElement('tr');
       var c = document.createElement('td');
-      c.textContent = JSON.stringify(items[i]);
+      var a = document.createElement('a');
+      a.setAttribute('href', items[i].canonical);
+      var img = new Image();
+      img.setAttribute('src', icons[items[i].source]);
+      c.appendChild(img);
+      a.textContent = nice_description(items[i]);
+      c.appendChild(a);
       r.appendChild(c);
       t.appendChild(r);
     }
